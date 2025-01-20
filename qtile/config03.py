cbb93aa6eba7df42c743b8ae43e13db82ad9000d
Thiago Pereira <thiagopereiramail@gmail.com>
2025-01-20 15:30:02 -0300

    wip: Testar a modificação da configuração 03

--- qtile/config03.py
+++ qtile/config03.py
@@ -2,6 +2,43 @@ from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 
+
+# QTile - Default Settings
+wmname                     = "LG3D"
+auto_fullscreen            = True
+auto_minimize              = True
+reconfigure_screens        = True
+bring_front_click          = False
+cursor_warp                = False
+floats_kept_above          = True
+follow_mouse_focus         = True
+dgroups_key_binder         = None
+focus_on_window_activation = "smart"
+dgroups_app_rules          = []
+
+widget_defaults = dict(
+  font="Ubuntu Bold",
+  fontsize=12,
+  padding=0,
+)
+
+extension_defaults = widget_defaults.copy()
+
+floating_layout = layout.Floating(
+  float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class="confirmreset"),  # gitk
+    Match(wm_class="makebranch"),  # gitk
+    Match(wm_class="maketag"),  # gitk
+    Match(wm_class="ssh-askpass"),  # ssh-askpass
+    Match(title="branchdialog"),  # gitk
+    Match(title="pinentry"),  # GPG key password entry
+  ]
+)
+
+
+# My Settings
 _mod      = "mod4"
 _terminal = "lxterminal"
 _app_launcher = "rofi -show drun -show-icons"
@@ -40,24 +77,11 @@ keys = [
   Key([_mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
   # Modify layout or window style
-  Key([_mod], "m", lazy.layout.maximize(), desc="Toggle between min and max sizes"),
+  Key([_mod], "m", lazy.next_layout(), desc="Toggle between layouts"),
 
   # Hide/Show tool bar
   Key([_mod], "b", lazy.hide_show_bar(position='all'), desc="Toggles the bar to show/hide"),
 ]
-
-# Add key bindings to switch VTs in Wayland.
-# We can't check qtile.core.name in default config as it is loaded before qtile is started
-# We therefore defer the check until the key binding is run by using .when(func=...)
-# for vt in range(1, 8):
-#     keys.append(
-#         Key(
-#             ["control", "mod1"],
-#             f"f{vt}",
-#             lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
-#             desc=f"Switch to VT{vt}",
-#         )
-#     )
 
 
 groups = [Group(i) for i in "1234"]
@@ -102,13 +126,6 @@ layouts = [
     # layout.Zoomy(),
 ]
 
-widget_defaults = dict(
-    font="Ubuntu Bold",
-    fontsize=12,
-    padding=0,
-)
-
-extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
@@ -149,46 +166,3 @@ mouse = [
     Drag([_mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([_mod], "Button2", lazy.window.bring_to_front()),
 ]
-
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
-bring_front_click = False
-floats_kept_above = True
-cursor_warp = False
-floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-    ]
-)
-auto_fullscreen = True
-focus_on_window_activation = "smart"
-reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
-auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
-
-# xcursor theme (string or None) and size (integer) for Wayland backend
-wl_xcursor_theme = None
-wl_xcursor_size = 24
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
