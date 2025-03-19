01b13f8a20c5a0b0b0fcce4f14abd204d303c3e8
Thiago Pereira <thiagopereiramail@gmail.com>
2025-03-19 08:05:16 -0300

    fix: Remover o pacote lxclipboard

--- makefiles/debian-minimal.sh
+++ makefiles/debian-minimal.sh
@@ -4,7 +4,7 @@ deps_install:
 	sudo apt update
 	sudo apt install -y xorg lxde-core lxappearance i3 xinit lightdm lightdm-gtk-greeter\
 		picom lxrandr lxtask pulseaudio pulseaudio-utils pavucontrol bluetooth blueman\
-		lxclipboard rofi bash-completion neovim alarm-clock-applet xrdp
+		rofi bash-completion neovim alarm-clock-applet xrdp
 	sudo apt remove -y openbox openbox-lxde-session
 	sudo apt autoremove -y
 
