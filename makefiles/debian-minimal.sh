eef33c294d82ef8e5dda6f1d690936cd0503fcc9
Thiago Pereira <thiagopereiramail@gmail.com>
2025-04-06 14:29:35 -0300

    Adicionar o script de instalação do docker

--- makefiles/debian-minimal.sh
+++ makefiles/debian-minimal.sh
@@ -38,6 +38,33 @@ remote_access_configure:
 	sudo systemctl enable xrdp
 
 
+install_docker:
+	# Add Docker's official GPG key:
+	sudo apt-get update
+	sudo apt-get install ca-certificates curl
+	sudo install -m 0755 -d /etc/apt/keyrings
+	sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+	# Add the repository to Apt sources:
+	echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+	
+	sudo apt-get update
+
+	# Install the Docker packages:
+	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+	# Linux post-installation steps
+	sudo groupadd docker
+	sudo usermod -aG docker $USER
+
+	# Log out and log back to reload configurations
+	# To test installation: docker run hello-world
+
+
 test:
 	@echo "configure debian minimal"
 	@pwd
